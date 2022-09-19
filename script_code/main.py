2f374ddd94a57052126d7df71a9727e9dd1b9115
Ricardo de Matos <ricardo.dematos@valispace.com>
2022-09-20 00:32:21 +0100

    Code format errors reported by black fixed

--- script_code/main.py
+++ script_code/main.py
@@ -5,4 +5,4 @@ def main(*args, **kwargs) -> Dict[str, Any]:
     """
     This file and folder will be replaced when container is built.
     """
-    return {'result': 'Hello World!'}
+    return {"result": "Hello World!"}
