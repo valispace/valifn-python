702a4623acb6a852d1fa03809bda0d4b86542c5f
antonio-valispace <78161508+antonio-valispace@users.noreply.github.com>
2023-02-05 21:12:00 +0000

    VALI-5456 :: Properly Document Existing Scripting Module Scripts (#43)
    Co-authored-by: Ricardo de Matos <108014795+ricardo-dematos@users.noreply.github.com>

--- templates/testdashboard.py
+++ templates/testdashboard.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+
+"""
+    Can take input from automation or be triggered manually
+    Returns string placed on predefined Dashboard 
+    text blocks with calculated test run statistics.
+"""
+
+__author__ = "Gonçalo Ivo"
+__copyright__ = "Copyright 2022, Valispace"
+__credits__ = ["Gonçalo Ivo"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Valispace"
+__email__ = "support@valispace.com"
+__status__ = "Development"
+
 import csv, json
 from valispace import API
 
